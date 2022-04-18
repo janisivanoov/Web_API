a924b46a1719d7be09a376a2a0f7a90e40a5069c
janisivanoov <janisivanov@icloud.com>
2022-04-18 11:19:23 +0300

    Add project files.

---
+++ WebAPIProject/Configuration/WelcomeWebAPI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+namespace WebAPIProject.Configuration
+{
+    public class WelcomeWebAPI
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "DefaultAPI",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+        }
+    }
+}
---
+++ WebAPIProject/Controllers/WelcomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Http;
+
+namespace WebAPIProject.Controllers
+{
+    public class WelcomeController : ApiController
+    {
+        public string Get()
+        {
+            string strWelcome = "Hello World";
+            return strWelcome;
+        }
+    }
+}
